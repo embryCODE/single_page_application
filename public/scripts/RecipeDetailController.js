9a37e1264238d5ae479c66ccb57f9f0842b7dc87
Mason Embry <mason@embrycode.com>
2016-11-19 12:16:07 -0500

    Adding recipes works.

--- public/scripts/RecipeDetailController.js
+++ public/scripts/RecipeDetailController.js
@@ -6,28 +6,63 @@
 
   .controller('RecipeDetailController', function($scope, dataService, $location, $routeParams) {
 
-      /** Get recipe by id in url, if one exists. */
-      if ($routeParams.id) {
-        dataService.getRecipeById($routeParams.id, function(response) {
-          $scope.recipe = response.data;
+    /** Gets all categories on page load. */
+    dataService.getAllCategories(function(response) {
+      $scope.allCategories = response.data;
+    });
+
+    /** Gets all food items on page load. */
+    dataService.getAllFoodItems(function(response) {
+      $scope.allFoodItems = response.data;
+    });
+
+    /** Get recipe by id in url, if one exists. */
+    if ($routeParams.id) {
+      dataService.getRecipeById($routeParams.id, function(response) {
+        $scope.recipe = response.data;
+      });
+    } else {
+      $scope.recipe = {};
+      $scope.recipe.ingredients = [];
+      $scope.recipe.steps = [];
+    }
+
+    /** Cancels and clears recipeData. */
+    $scope.cancel = function() {
+      $scope.recipe = {};
+      $location.path('/');
+    };
+
+    /** Deletes an item from the ingredient or step list. */
+    $scope.deleteItem = function(array, index) {
+      array.splice(index, 1);
+    };
+
+    /** Adds an item to the ingredient or steps list. */
+    $scope.addIngredient = function() {
+      $scope.recipe.ingredients.push({item: '', condition: '', amount: ''});
+    };
+
+    /** Adds an item to the ingredient or steps list. */
+    $scope.addStep = function() {
+      $scope.recipe.steps.push({description: ''});
+    };
+
+
+    $scope.saveRecipe = function() {
+
+      if (true) {
+        dataService.addRecipe($scope.recipe, function() {
+          $location.path('/');
         });
+      } else {
+        // dataService.updateRecipeById($scope.recipe._id, $scope.recipe, function() {
+        //   $location.path('/');
+        // });
       }
 
-      /** Cancels and clears recipeData. */
-      $scope.cancel = function() {
-        $scope.recipe = {};
-        $location.path('/');
-      };
 
-      /** Gets all categories on page load. */
-      dataService.getAllCategories(function(response) {
-        $scope.allCategories = response.data;
-      });
-
-      /** Gets all food items on page load. */
-      dataService.getAllFoodItems(function(response) {
-        $scope.allFoodItems = response.data;
-      });
+    };
 
   });
 
--- public/scripts/dataService.js
+++ public/scripts/dataService.js
@@ -6,42 +6,42 @@
 
     this.getAllRecipes = function(successCallback, errorCallback) {
       $http.get('/api/recipes')
-      .then(successCallback, errorCallback);
+        .then(successCallback, errorCallback);
     };
 
     this.getRecipesInCategory = function(categoryName, successCallback, errorCallback) {
       $http.get('/api/recipes?category=' + categoryName)
-      .then(successCallback, errorCallback);
+        .then(successCallback, errorCallback);
     };
 
     this.getRecipeById = function(id, successCallback, errorCallback) {
       $http.get('/api/recipes/' + id)
-      .then(successCallback, errorCallback);
+        .then(successCallback, errorCallback);
     };
 
-    this.updateRecipeById = function(id, successCallback, errorCallback) {
-      $http.put('/api/recipes/' + id)
-      .then(successCallback, errorCallback);
+    this.updateRecipeById = function(id, recipeObject, successCallback, errorCallback) {
+      $http.put('/api/recipes/' + id, recipeObject)
+        .then(successCallback, errorCallback);
     };
 
-    this.addRecipe = function(successCallback, errorCallback) {
-      $http.post('/api/recipes')
-      .then(successCallback, errorCallback);
+    this.addRecipe = function(recipeObject, successCallback, errorCallback) {
+      $http.post('/api/recipes/', recipeObject)
+        .then(successCallback, errorCallback);
     };
 
     this.deleteRecipeById = function(id, successCallback, errorCallback) {
       $http.delete('/api/recipes/' + id)
-      .then(successCallback, errorCallback);
+        .then(successCallback, errorCallback);
     };
 
     this.getAllCategories = function(successCallback, errorCallback) {
       $http.get('/api/categories')
-      .then(successCallback, errorCallback);
+        .then(successCallback, errorCallback);
     };
 
     this.getAllFoodItems = function(successCallback, errorCallback) {
       $http.get('/api/fooditems')
-      .then(successCallback, errorCallback);
+        .then(successCallback, errorCallback);
     };
 
   });
